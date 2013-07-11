aa19e8822a4496db7f423a7feabe5b4cd9299523
Andreas <me@grml.in>
2013-07-11 16:51:33 +0200

    Gizmo sample

--- js/gremlins/Gizmo.js
+++ js/gremlins/Gizmo.js
@@ -1,5 +1,20 @@
-var Gizmo = GremlinJS.define('Gizmo',
-    function () {
-        this.el.innerHTML = '<strong>Hello World, Gizmo here!</strong>';
+var Gizmo = GremlinJS.define('Gizmo', function () {
+    this.el.innerHTML = '<strong>Hello World, Gizmo here! Loading some images from google now...</strong>';
+    this._search();
+}, {
+    elements: {
+
+    },
+    _search: function(){
+        $.ajax({
+            url: Gizmo.G_IMG_SEARCH_API,
+            cache: false,
+            dataType: 'jsonp',
+            success: function(response){
+                console.dir(response);
+            }
+        });
     }
-);
+}, {
+    G_IMG_SEARCH_API: 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=gizmo'
+});
