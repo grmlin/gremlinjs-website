708843d9c0a309ba7d1c090201fafa3021e96d90
Andreas <me@grml.in>
2013-07-26 12:19:42 +0200

    up arrow

--- js/site.js
+++ js/site.js
@@ -1,17 +1,24 @@
-$(function(){
-	$('body').on('flatdoc:ready', function(){
-		$('.content h3').each(function(){
-			var link = document.createElement('a');
-			link.href = '#' + this.id;
-			link.textContent = '#';
+$(function () {
+  $('body')
+    .on('flatdoc:ready', function () {
+      $('.content h3').each(function () {
+        var link = document.createElement('a');
+        link.href = '#' + this.id;
+        link.textContent = '#';
 
-			this.innerHTML = this.textContent.replace(/^(.*)(\.|#)/gi,'$2');
-			//console.log(link.href)
-			$(this).append(link);
-		});
+        this.innerHTML = this.textContent.replace(/^(.*)(\.|#)/gi, '$2');
+        //console.log(link.href)
+        $(this).append(link);
+      });
 
-		$('.menu ul.level-3 li a').each(function(){
-			this.innerHTML = this.textContent.replace(/^(.*)(\.|#)/gi,'$2');
-		});	
-	});
+      $('.menu ul.level-3 li a').each(function () {
+        this.innerHTML = this.textContent.replace(/^(.*)(\.|#)/gi, '$2');
+      });
+    })
+    .on('click', '.top-link', function (event) {
+      event.preventDefault();
+      $('html, body').animate({ scrollTop: 0}, 250);
+    });
+
+
 });
